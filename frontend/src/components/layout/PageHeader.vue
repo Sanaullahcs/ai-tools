a1364608f695971c3d35d76836b38b701f7c1c1e
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-04-16 12:23:34 +0500

    Code Updated

--- frontend/src/components/layout/PageHeader.vue
+++ frontend/src/components/layout/PageHeader.vue
@@ -47,7 +47,42 @@ export default {
 
 <style scoped>
 .cmn-banner {
+  /* --------------------------- */
   /* background-image: url('~@/assets/images/banner/banner-bg.png'); */
-  background: #111827;
+  /* background: #111827; */
+  /* ---------------------------------- */
+  /* ------------------------------------ */
+  /* background: linear-gradient(-45deg, #111827, #111827, #e0013f, #111827);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+}
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  } */
+  /* --------------------------------- */
+  /* -------------------------------- */
+  animation-name: changeBackground;
+  animation-duration: 20s; /* Adjust as needed */
+  animation-iteration-count: infinite;
+  background-size: cover;
+  background-position: center;
+  animation-timing-function: linear; /* Change for different easing */
+
+  /* ----------------------------------- */
+}
+@keyframes changeBackground {
+  0% { background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('~@/assets/images/ai/ai1.jpg'); }
+  20% { background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('~@/assets/images/ai/ai2.jpg'); }
+  40% { background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('~@/assets/images/ai/ai3.jpg'); }
+  60% { background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('~@/assets/images/ai/ai4.jpg'); }
+  80% { background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('~@/assets/images/ai/ai5.jpg'); }
+  100% { background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('~@/assets/images/ai/ai1.jpg'); }
 }
 </style>
--- frontend/src/components/page_components/home/Tour.vue
+++ frontend/src/components/page_components/home/Tour.vue
@@ -1,27 +1,31 @@
 <template>
-    <div class="section tour tour-two pb-0">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="tour__content wow fadeInUp" data-wow-duration="600ms" data-wow-delay="300ms">
-                        <video muted loop autoplay>
+  <div class="section tour tour-two pb-0">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div
+            class="tour__content wow fadeInUp"
+            data-wow-duration="600ms"
+            data-wow-delay="300ms"
+          >
+            <video muted loop autoplay>
                             <source :src="require('@/assets/images/video/video.mp4')" type="video/mp4">
                         </video>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-        <div class="anime anime--dark">
-            <img :src="require('@/assets/images/generate/anime.png')" alt="Image">
-        </div>
-        <div class="anime anime--light">
-            <img :src="require('@/assets/images/anime-five.png')" alt="Image">
-        </div>
+      </div>
     </div>
+    <div class="anime anime--dark">
+      <img :src="require('@/assets/images/generate/anime.png')" alt="Image" />
+    </div>
+    <div class="anime anime--light">
+      <img :src="require('@/assets/images/anime-five.png')" alt="Image" />
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Tour",
-    };
+export default {
+  name: "Tour",
+};
 </script>
